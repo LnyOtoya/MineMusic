ece09d774d6859d5685cd06c6b07211e283f2c74
LnyOtoya <zombie11019@outlook.com>
2026-02-21 17:34:37 +0800

    final fix api lastfm

--- lib/utils/lastfm_config.dart
+++ lib/utils/lastfm_config.dart
@@ -1,6 +1,6 @@
 class LastFMConfig {
-  static String get apiKey => String.fromEnvironment('LASTFM_API_KEY', defaultValue: '');
-  static String get sharedSecret => String.fromEnvironment('LASTFM_SHARED_SECRET', defaultValue: '');
+  static const String apiKey = String.fromEnvironment('LASTFM_API_KEY', defaultValue: '');
+  static const String sharedSecret = String.fromEnvironment('LASTFM_SHARED_SECRET', defaultValue: '');
 
   static bool get isConfigured => apiKey.isNotEmpty && sharedSecret.isNotEmpty;
 }
