afd6a3024d11af6e1f94f90c9f0852923bdb94c1
LnyOtoya <zombie11019@outlook.com>
2026-02-22 16:50:00 +0800

    update version

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -50,7 +50,7 @@ class AboutPage extends StatelessWidget {
             ),
             const SizedBox(height: 8),
             Text(
-              '版本 1.2.1',
+              '版本 1.2.2',
               style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                 color: Theme.of(context).colorScheme.onSurfaceVariant,
               ),
